1cde6a33467ae60dde004cce4b8d65f3ec1ed8fd
merengue <therealmerengue@gmail.com>
2023-01-14 20:28:12 +0100

    all core modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,8 @@ dependencyResolutionManagement {
 rootProject.name = "DayLighter"
 include(":app")
 include(":core:common")
+include(":core:network")
+include(":core:data")
+include(":core:database")
+include(":core:domain")
+include(":core:ui")
