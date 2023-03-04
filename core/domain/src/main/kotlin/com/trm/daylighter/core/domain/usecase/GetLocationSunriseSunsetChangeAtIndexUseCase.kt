40420c5b25ab79e876d0f20594f47fafc0d729d0
tosoba <therealmerengue@gmail.com>
2023-03-04 15:37:01 +0100

    Revert "using a separate enqueue sync use case"
    This reverts commit 1b09f6b0136fb6535a895d898893d66880c7cb5d.

--- core/domain/src/main/kotlin/com/trm/daylighter/core/domain/usecase/GetLocationSunriseSunsetChangeAtIndexUseCase.kt
+++ core/domain/src/main/kotlin/com/trm/daylighter/core/domain/usecase/GetLocationSunriseSunsetChangeAtIndexUseCase.kt
@@ -2,22 +2,26 @@ package com.trm.daylighter.core.domain.usecase
 
 import com.trm.daylighter.core.domain.model.*
 import com.trm.daylighter.core.domain.repo.SunriseSunsetRepo
+import com.trm.daylighter.core.domain.work.SyncWorkManager
 import javax.inject.Inject
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onEach
 
 class GetLocationSunriseSunsetChangeAtIndexUseCase
 @Inject
-constructor(private val repo: SunriseSunsetRepo) {
-  operator fun invoke(index: Int): Flow<Loadable<LocationSunriseSunsetChange>> = flow {
-    emit(LoadingFirst)
-    try {
-      emit(repo.getLocationSunriseSunsetChangeAtIndex(index).asLoadable())
-    } catch (ex: CancellationException) {
-      throw ex
-    } catch (ex: Exception) {
-      emit(FailedFirst(ex))
-    }
-  }
+constructor(private val repo: SunriseSunsetRepo, private val manager: SyncWorkManager) {
+  operator fun invoke(index: Int): Flow<Loadable<LocationSunriseSunsetChange>> =
+    flow {
+        emit(LoadingFirst)
+        try {
+          emit(repo.getLocationSunriseSunsetChangeAtIndex(index).asLoadable())
+        } catch (ex: CancellationException) {
+          throw ex
+        } catch (ex: Exception) {
+          emit(FailedFirst(ex))
+        }
+      }
+      .onEach { loadable -> if (loadable is Ready) manager.enqueueSync() }
 }
