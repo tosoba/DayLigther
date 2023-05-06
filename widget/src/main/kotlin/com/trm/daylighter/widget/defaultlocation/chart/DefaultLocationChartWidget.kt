ab470914ca05f5c59733d7aa302f0a96b11becd9
merengue <therealmerengue@gmail.com>
2023-05-06 09:23:13 +0200

    move chart widget funs outside of class

--- widget/src/main/kotlin/com/trm/daylighter/widget/defaultlocation/chart/DefaultLocationChartWidget.kt
+++ widget/src/main/kotlin/com/trm/daylighter/widget/defaultlocation/chart/DefaultLocationChartWidget.kt
@@ -47,8 +47,7 @@ import com.trm.daylighter.widget.ui.RetryButton
 import com.trm.daylighter.widget.ui.appWidgetBackgroundCornerRadius
 import com.trm.daylighter.widget.ui.stringResource
 import com.trm.daylighter.widget.ui.toPx
-import com.trm.daylighter.widget.util.ext.antialiasPaint
-import com.trm.daylighter.widget.util.ext.lazyPaint
+import com.trm.daylighter.widget.util.ext.antiAliasPaint
 import java.time.Duration
 import java.time.ZonedDateTime
 
@@ -56,169 +55,169 @@ class DefaultLocationChartWidget : GlanceAppWidget() {
   override val stateDefinition = DefaultLocationChartWidgetStateDefinition
   override val sizeMode: SizeMode = SizeMode.Responsive(setOf(wideMode))
 
-  private val nightPaint by lazyPaint(color = nightColor.toArgb())
-  private val astronomicalTwilightPaint by lazyPaint(color = astronomicalTwilightColor.toArgb())
-  private val nauticalTwilightPaint by lazyPaint(color = nauticalTwilightColor.toArgb())
-  private val civilTwilightPaint by lazyPaint(color = civilTwilightColor.toArgb())
-  private val dayPaint by lazyPaint(color = dayColor.toArgb())
-  private val timelinePaint by lazyPaint(color = light_onDayColor.toArgb())
-
   @Composable
   override fun Content() {
     GlanceTheme {
       when (val change = currentState<Loadable<LocationSunriseSunsetChange>>()) {
         Empty -> AddLocationButton()
         is Loading -> CircularProgressIndicator()
-        is Ready -> DayChart(change = change)
+        is Ready -> DayChart(change = change.data)
         is Failed -> RetryButton(onClick = updateWidgetAction())
       }
     }
   }
 
-  @Composable
-  private fun DayChart(change: Ready<LocationSunriseSunsetChange>) {
-    Box(
-      contentAlignment = Alignment.TopEnd,
-      modifier = GlanceModifier.fillMaxSize().appWidgetBackgroundCornerRadius()
-    ) {
-      Image(
-        provider = BitmapImageProvider(dayChartBitmap(change.data)),
-        contentDescription = null,
-        contentScale = ContentScale.FillBounds,
-        modifier = GlanceModifier.fillMaxSize()
-      )
-
-      Image(
-        provider = ImageProvider(commonR.drawable.refresh),
-        contentDescription = stringResource(id = commonR.string.refresh),
-        modifier = GlanceModifier.padding(5.dp).clickable(updateWidgetAction())
-      )
-    }
-  }
-
-  @Composable
-  private fun dayChartBitmap(change: LocationSunriseSunsetChange): Bitmap {
-    val size = LocalSize.current
-    val widthPx = size.width.value.toPx
-    val heightPx = size.height.value.toPx
-    val bitmap = Bitmap.createBitmap(widthPx.toInt(), heightPx.toInt(), Bitmap.Config.ARGB_8888)
-
-    Canvas(bitmap).apply {
-      DrawDayPeriods(today = change.today)
-      DrawNowLine(today = change.today)
-      val timelineTop = heightPx - minOf(5.dp.value.toPx, 25f)
-      DrawTimeLine(dateTime = change.today.sunrise, paint = timelinePaint, top = timelineTop)
-      DrawTimeLine(dateTime = change.today.sunset, paint = timelinePaint, top = timelineTop)
-    }
-
-    return bitmap
-  }
-
-  @Composable
-  private fun Canvas.DrawDayPeriods(today: SunriseSunset) {
-    val size = LocalSize.current
-    val widthPx = size.width.value.toPx
-    val heightPx = size.height.value.toPx
-
-    val secondsInDay = Duration.ofDays(1L).seconds.toFloat()
-    val durations = dayPeriodDurationsInSeconds(today)
-    val paints = dayPeriodPaints()
-
-    var left = 0f
-    var right = left + durations.first() / secondsInDay * widthPx
-    durations.indices.forEach { index ->
-      drawRect(left, 0f, right, heightPx, paints[index])
-      left += durations[index] / secondsInDay * widthPx
-      if (index != durations.lastIndex) right += durations[index + 1] / secondsInDay * widthPx
-      else right = widthPx
-    }
-  }
-
-  @Composable
-  private fun Canvas.DrawNowLine(today: SunriseSunset) {
-    val context = LocalContext.current
-    DrawTimeLine(
-      dateTime = ZonedDateTime.now(today.sunrise.zone),
-      paint =
-        antialiasPaint(
-          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-              Color(context.resources.getColor(commonR.color.sun_outline, context.theme))
-            } else {
-              Color(context.resources.getColor(commonR.color.sun_outline))
-            }
-            .toArgb()
-        )
-    )
-  }
-
-  @Composable
-  private fun Canvas.DrawTimeLine(
-    dateTime: ZonedDateTime,
-    paint: Paint,
-    top: Float = 0f,
-    bottom: Float = LocalSize.current.height.value.toPx,
-    width: Dp = 2.dp
-  ) {
-    val widthPx = LocalSize.current.width.value.toPx
-    val secondsInDay = Duration.ofDays(1L).seconds.toFloat()
-    val startOfDay = dateTime.toLocalDate().atStartOfDay(dateTime.zone)
-    val secondOfDay = Duration.between(startOfDay, dateTime).seconds.toFloat()
-    val linePosition = secondOfDay / secondsInDay * widthPx
-    val lineWidth = width.value.toPx
-    drawRect(linePosition - lineWidth / 2f, top, linePosition + lineWidth / 2f, bottom, paint)
-  }
-
-  private fun dayPeriodDurationsInSeconds(sunriseSunset: SunriseSunset): List<Float> {
-    val (
-      astronomicalTwilightBegin,
-      astronomicalTwilightEnd,
-      civilTwilightBegin,
-      civilTwilightEnd,
-      _,
-      nauticalTwilightBegin,
-      nauticalTwilightEnd,
-      _,
-      sunrise,
-      sunset,
-      date) =
-      sunriseSunset
-    val periodInstants =
-      listOf(
-          date.atStartOfDay(sunrise.zone),
-          astronomicalTwilightBegin,
-          nauticalTwilightBegin,
-          civilTwilightBegin,
-          sunrise,
-          sunset,
-          civilTwilightEnd,
-          nauticalTwilightEnd,
-          astronomicalTwilightEnd,
-          date.atStartOfDay(sunrise.zone).plusDays(1L),
-        )
-        .map(ZonedDateTime::toInstant)
-    return periodInstants.indices.drop(1).map { index ->
-      Duration.between(periodInstants[index - 1], periodInstants[index]).seconds.toFloat()
-    }
-  }
-
-  private fun dayPeriodPaints(): List<Paint> =
-    listOf(
-      nightPaint,
-      astronomicalTwilightPaint,
-      nauticalTwilightPaint,
-      civilTwilightPaint,
-      dayPaint,
-      civilTwilightPaint,
-      nauticalTwilightPaint,
-      astronomicalTwilightPaint,
-      nightPaint
-    )
-
-  @Composable
-  private fun updateWidgetAction() =
-    actionSendBroadcast(DefaultLocationChartWidgetReceiver.updateIntent(LocalContext.current))
-
   companion object {
     private val wideMode = DpSize(200.dp, 50.dp)
   }
+}
+
+@Composable
+private fun DayChart(change: LocationSunriseSunsetChange) {
+  Box(
+    contentAlignment = Alignment.TopEnd,
+    modifier = GlanceModifier.fillMaxSize().appWidgetBackgroundCornerRadius()
+  ) {
+    Image(
+      provider = BitmapImageProvider(dayChartBitmap(change = change)),
+      contentDescription = null,
+      contentScale = ContentScale.FillBounds,
+      modifier = GlanceModifier.fillMaxSize()
+    )
+
+    Image(
+      provider = ImageProvider(commonR.drawable.refresh),
+      contentDescription = stringResource(id = commonR.string.refresh),
+      modifier = GlanceModifier.padding(5.dp).clickable(updateWidgetAction())
+    )
+  }
+}
+
+@Composable
+private fun Canvas.DrawNowLine(today: SunriseSunset) {
+  val context = LocalContext.current
+  DrawTimeLine(
+    dateTime = ZonedDateTime.now(today.sunrise.zone),
+    paint =
+      antiAliasPaint(
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Color(context.resources.getColor(commonR.color.sun_outline, context.theme))
+          } else {
+            Color(context.resources.getColor(commonR.color.sun_outline))
+          }
+          .toArgb()
+      )
+  )
+}
+
+@Composable
+private fun Canvas.DrawTimeLine(
+  dateTime: ZonedDateTime,
+  paint: Paint,
+  top: Float = 0f,
+  bottom: Float = LocalSize.current.height.value.toPx,
+  width: Dp = 2.dp
+) {
+  val widthPx = LocalSize.current.width.value.toPx
+  val secondsInDay = Duration.ofDays(1L).seconds.toFloat()
+  val startOfDay = dateTime.toLocalDate().atStartOfDay(dateTime.zone)
+  val secondOfDay = Duration.between(startOfDay, dateTime).seconds.toFloat()
+  val linePosition = secondOfDay / secondsInDay * widthPx
+  val lineWidth = width.value.toPx
+  drawRect(linePosition - lineWidth / 2f, top, linePosition + lineWidth / 2f, bottom, paint)
+}
+
+@Composable
+private fun updateWidgetAction() =
+  actionSendBroadcast(DefaultLocationChartWidgetReceiver.updateIntent(LocalContext.current))
+
+@Composable
+private fun dayChartBitmap(change: LocationSunriseSunsetChange): Bitmap {
+  val size = LocalSize.current
+  val widthPx = size.width.value.toPx
+  val heightPx = size.height.value.toPx
+  val bitmap = Bitmap.createBitmap(widthPx.toInt(), heightPx.toInt(), Bitmap.Config.ARGB_8888)
+  val timelinePaint = antiAliasPaint(color = light_onDayColor.toArgb())
+
+  Canvas(bitmap).apply {
+    DrawDayPeriods(today = change.today)
+    DrawNowLine(today = change.today)
+    val timelineTop = heightPx - minOf(5.dp.value.toPx, 25f)
+    DrawTimeLine(dateTime = change.today.sunrise, paint = timelinePaint, top = timelineTop)
+    DrawTimeLine(dateTime = change.today.sunset, paint = timelinePaint, top = timelineTop)
+  }
+
+  return bitmap
+}
+
+@Composable
+private fun Canvas.DrawDayPeriods(today: SunriseSunset) {
+  val size = LocalSize.current
+  val widthPx = size.width.value.toPx
+  val heightPx = size.height.value.toPx
+
+  val secondsInDay = Duration.ofDays(1L).seconds.toFloat()
+  val durations = dayPeriodDurationsInSeconds(today)
+  val paints = dayPeriodPaints()
+
+  var left = 0f
+  var right = left + durations.first() / secondsInDay * widthPx
+  durations.indices.forEach { index ->
+    drawRect(left, 0f, right, heightPx, paints[index])
+    left += durations[index] / secondsInDay * widthPx
+    if (index != durations.lastIndex) right += durations[index + 1] / secondsInDay * widthPx
+    else right = widthPx
+  }
+}
+
+private fun dayPeriodDurationsInSeconds(sunriseSunset: SunriseSunset): List<Float> {
+  val (
+    astronomicalTwilightBegin,
+    astronomicalTwilightEnd,
+    civilTwilightBegin,
+    civilTwilightEnd,
+    _,
+    nauticalTwilightBegin,
+    nauticalTwilightEnd,
+    _,
+    sunrise,
+    sunset,
+    date) =
+    sunriseSunset
+  val periodInstants =
+    listOf(
+        date.atStartOfDay(sunrise.zone),
+        astronomicalTwilightBegin,
+        nauticalTwilightBegin,
+        civilTwilightBegin,
+        sunrise,
+        sunset,
+        civilTwilightEnd,
+        nauticalTwilightEnd,
+        astronomicalTwilightEnd,
+        date.atStartOfDay(sunrise.zone).plusDays(1L),
+      )
+      .map(ZonedDateTime::toInstant)
+  return periodInstants.indices.drop(1).map { index ->
+    Duration.between(periodInstants[index - 1], periodInstants[index]).seconds.toFloat()
+  }
+}
+
+private fun dayPeriodPaints(): List<Paint> {
+  val nightPaint = antiAliasPaint(color = nightColor.toArgb())
+  val astronomicalTwilightPaint = antiAliasPaint(color = astronomicalTwilightColor.toArgb())
+  val nauticalTwilightPaint = antiAliasPaint(color = nauticalTwilightColor.toArgb())
+  val civilTwilightPaint = antiAliasPaint(color = civilTwilightColor.toArgb())
+  val dayPaint = antiAliasPaint(color = dayColor.toArgb())
+  return listOf(
+    nightPaint,
+    astronomicalTwilightPaint,
+    nauticalTwilightPaint,
+    civilTwilightPaint,
+    dayPaint,
+    civilTwilightPaint,
+    nauticalTwilightPaint,
+    astronomicalTwilightPaint,
+    nightPaint
+  )
 }
--- widget/src/main/kotlin/com/trm/daylighter/widget/util/ext/DrawExt.kt
+++ widget/src/main/kotlin/com/trm/daylighter/widget/util/ext/DrawExt.kt
@@ -2,8 +2,5 @@ package com.trm.daylighter.widget.util.ext
 
 import android.graphics.Paint
 
-internal fun lazyPaint(color: Int): Lazy<Paint> =
-  lazy(LazyThreadSafetyMode.NONE) { antialiasPaint(color) }
-
-internal fun antialiasPaint(color: Int): Paint =
+internal fun antiAliasPaint(color: Int): Paint =
   Paint(Paint.ANTI_ALIAS_FLAG).also { it.color = color }
