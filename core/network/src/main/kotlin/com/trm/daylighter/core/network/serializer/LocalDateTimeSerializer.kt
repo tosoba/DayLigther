cd21a1bb39f050ccba7ef9d76c72248b8f290e74
merengue <therealmerengue@gmail.com>
2023-05-21 11:59:59 +0200

    zonedDateTime -> localDateTime runs without timezone adjustments

--- core/network/src/main/kotlin/com/trm/daylighter/core/network/serializer/LocalDateTimeSerializer.kt
+++ core/network/src/main/kotlin/com/trm/daylighter/core/network/serializer/LocalDateTimeSerializer.kt
@@ -1,12 +1,14 @@
 package com.trm.daylighter.core.network.serializer
 
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import java.time.ZonedDateTime
 
 object LocalDateTimeSerializer : KSerializer<LocalDateTime> {
   override val descriptor: SerialDescriptor =
@@ -17,5 +19,5 @@ object LocalDateTimeSerializer : KSerializer<LocalDateTime> {
   }
 
   override fun deserialize(decoder: Decoder): LocalDateTime =
-    LocalDateTime.parse(decoder.decodeString())
+    ZonedDateTime.parse(decoder.decodeString()).toLocalDateTime()
 }
