fe65b60e1c48a3cd51ac6b6cd7ca4b6ef1994255
Tomasz Osoba <tomasz.osoba@knowit.eu>
2023-06-29 10:07:58 +0200

    using OutlineButton in InfoButtonCard

--- core/ui/src/main/kotlin/com/trm/daylighter/core/ui/composable/InfoButtonCard.kt
+++ core/ui/src/main/kotlin/com/trm/daylighter/core/ui/composable/InfoButtonCard.kt
@@ -5,8 +5,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -31,7 +31,7 @@ fun InfoButtonCard(
     ) {
       Text(text = infoText, fontSize = 20.sp, textAlign = TextAlign.Center)
       Spacer(modifier = Modifier.height(10.dp))
-      Button(onClick = onButtonClick) { Text(text = actionText) }
+      OutlinedButton(onClick = onButtonClick) { Text(text = actionText) }
     }
   }
 }
