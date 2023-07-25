e7aa78e98d415eaed943587105576bf1bb605b81
merengue <therealmerengue@gmail.com>
2023-07-25 21:42:51 +0200

    add copyright overlay

--- core/common/src/main/kotlin/com/trm/daylighter/core/common/util/ext/MapViewExt.kt
+++ core/common/src/main/kotlin/com/trm/daylighter/core/common/util/ext/MapViewExt.kt
@@ -1,10 +1,12 @@
 package com.trm.daylighter.core.common.util.ext
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import com.trm.daylighter.core.common.model.MapDefaults
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.CustomZoomButtonsController
 import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.CopyrightOverlay
 import org.osmdroid.views.overlay.TilesOverlay
 
 @SuppressLint("ClickableViewAccessibility")
@@ -16,10 +18,20 @@ fun MapView.setDefaultDisabledConfig(darkMode: Boolean) {
   isFlingEnabled = false
   setOnTouchListener { _, _ -> true }
   if (darkMode) overlayManager.tilesOverlay.setColorFilter(TilesOverlay.INVERT_COLORS)
+  addCopyrightOverlay(darkMode)
 }
 
 fun MapView.setPosition(latitude: Double, longitude: Double, zoom: Double) {
   controller.setZoom(zoom)
   mapOrientation = MapDefaults.ORIENTATION
   setExpectedCenter(GeoPoint(latitude, longitude))
+}
+
+fun MapView.addCopyrightOverlay(darkMode: Boolean) {
+  overlays.add(
+    CopyrightOverlay(context).apply {
+      setTextColor(if (darkMode) Color.WHITE else Color.BLACK)
+      setCopyrightNotice(tileProvider.tileSource.copyrightNotice)
+    }
+  )
 }
--- feature/location/src/main/kotlin/com/trm/daylighter/feature/location/util/MapViewExt.kt
+++ feature/location/src/main/kotlin/com/trm/daylighter/feature/location/util/MapViewExt.kt
@@ -1,6 +1,7 @@
 package com.trm.daylighter.feature.location.util
 
 import com.trm.daylighter.core.common.model.MapDefaults
+import com.trm.daylighter.core.common.util.ext.addCopyrightOverlay
 import com.trm.daylighter.feature.location.model.MapPosition
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.CustomZoomButtonsController
@@ -14,6 +15,7 @@ internal fun MapView.setDefaultConfig(darkMode: Boolean) {
   zoomController.setVisibility(CustomZoomButtonsController.Visibility.NEVER)
   minZoomLevel = MapDefaults.MIN_ZOOM
   if (darkMode) overlayManager.tilesOverlay.setColorFilter(TilesOverlay.INVERT_COLORS)
+  addCopyrightOverlay(darkMode)
 }
 
 internal fun MapView.restorePosition(position: MapPosition) {
