07a732e902762649ce8b03a65904d26c156fe1b5
tosoba <therealmerengue@gmail.com>
2023-09-18 21:26:41 +0200

    map position flow init test

--- feature/location/src/test/kotlin/com/trm/daylighter/feature/location/LocationViewModelTests.kt
+++ feature/location/src/test/kotlin/com/trm/daylighter/feature/location/LocationViewModelTests.kt
@@ -2,21 +2,30 @@ package com.trm.daylighter.feature.location
 
 import androidx.lifecycle.SavedStateHandle
 import app.cash.turbine.test
+import com.trm.daylighter.core.common.model.MapDefaults
+import com.trm.daylighter.core.domain.model.Location
 import com.trm.daylighter.core.domain.usecase.GetCurrentUserLatLngUseCase
 import com.trm.daylighter.core.domain.usecase.GetLocationById
 import com.trm.daylighter.core.domain.usecase.GetLocationDisplayName
 import com.trm.daylighter.core.domain.usecase.IsGeocodingEmailPreferenceSetFlowUseCase
 import com.trm.daylighter.core.domain.usecase.SaveLocationUseCase
+import com.trm.daylighter.core.testing.rule.MainDispatcherRule
 import com.trm.daylighter.feature.location.model.MapPosition
+import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
+import java.time.LocalDateTime
+import java.time.ZoneId
 import kotlin.test.assertEquals
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runCurrent
 import kotlinx.coroutines.test.runTest
+import org.junit.Rule
 import org.junit.Test
 
 class LocationViewModelTests {
+  @get:Rule val mainDispatcherRule = MainDispatcherRule()
+
   @Test
   fun `GIVEN no initial location id THEN map position flow should only emit default map position`() =
     runTest {
@@ -25,6 +34,42 @@ class LocationViewModelTests {
         assertMapPositionEquals(MapPosition(), awaitItem())
         cancelAndIgnoreRemainingEvents()
       }
+    }
+
+  @Test
+  fun `GIVEN an initial location id THEN map position flow should map position for location id`() =
+    runTest {
+      val expectedLocation =
+        Location(
+          latitude = 0.0,
+          longitude = 0.0,
+          name = "",
+          isDefault = false,
+          updatedAt = LocalDateTime.now(),
+          zoneId = ZoneId.systemDefault()
+        )
+
+      viewModel(
+          savedStateHandle = SavedStateHandle(mapOf(locationIdParam to 61L)),
+          getLocationById =
+            mockk<GetLocationById>().apply {
+              coEvery { this@apply(any()) } returns expectedLocation
+            }
+        )
+        .mapPositionFlow
+        .test {
+          runCurrent()
+          assertMapPositionEquals(
+            MapPosition(
+              latitude = expectedLocation.latitude,
+              longitude = expectedLocation.longitude,
+              zoom = MapDefaults.INITIAL_LOCATION_ZOOM,
+              label = expectedLocation.name
+            ),
+            awaitItem()
+          )
+          cancelAndIgnoreRemainingEvents()
+        }
     }
 
   private fun viewModel(
