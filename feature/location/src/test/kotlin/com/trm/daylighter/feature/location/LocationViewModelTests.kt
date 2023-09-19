bb1d616ffc0b3d54034b3832d9a8e2bef90f484a
tosoba <therealmerengue@gmail.com>
2023-09-19 21:13:36 +0200

    more location VM tests of public vals

--- feature/location/src/test/kotlin/com/trm/daylighter/feature/location/LocationViewModelTests.kt
+++ feature/location/src/test/kotlin/com/trm/daylighter/feature/location/LocationViewModelTests.kt
@@ -4,12 +4,14 @@ import androidx.lifecycle.SavedStateHandle
 import app.cash.turbine.test
 import com.trm.daylighter.core.common.model.MapDefaults
 import com.trm.daylighter.core.domain.model.Location
+import com.trm.daylighter.core.domain.model.Ready
 import com.trm.daylighter.core.domain.usecase.GetCurrentUserLatLngUseCase
 import com.trm.daylighter.core.domain.usecase.GetLocationById
 import com.trm.daylighter.core.domain.usecase.GetLocationDisplayName
 import com.trm.daylighter.core.domain.usecase.IsGeocodingEmailPreferenceSetFlowUseCase
 import com.trm.daylighter.core.domain.usecase.SaveLocationUseCase
 import com.trm.daylighter.core.testing.rule.MainDispatcherRule
+import com.trm.daylighter.feature.location.model.LocationScreenMode
 import com.trm.daylighter.feature.location.model.MapPosition
 import io.mockk.coEvery
 import io.mockk.every
@@ -27,6 +29,24 @@ class LocationViewModelTests {
   @get:Rule val mainDispatcherRule = MainDispatcherRule()
 
   @Test
+  fun `GIVEN no initial location id THEN screen mode is add`() {
+    assertEquals(LocationScreenMode.ADD, viewModel().screenMode)
+  }
+
+  @Test
+  fun `GIVEN an initial location id THEN screen mode is edit`() {
+    assertEquals(
+      LocationScreenMode.EDIT,
+      viewModel(
+          savedStateHandle = SavedStateHandle(mapOf(locationIdParam to 41L)),
+          getLocationById =
+            mockk<GetLocationById>().apply { coEvery { this@apply(any()) } returns null }
+        )
+        .screenMode
+    )
+  }
+
+  @Test
   fun `GIVEN no initial location id THEN map position flow should only emit default map position`() =
     runTest {
       viewModel().mapPositionFlow.test {
@@ -34,6 +54,22 @@ class LocationViewModelTests {
         assertMapPositionEquals(MapPosition(), awaitItem())
         cancelAndIgnoreRemainingEvents()
       }
+    }
+
+  @Test
+  fun `GIVEN an initial location id WHEN get location by id returns null THEN map position flow should only emit default map position`() =
+    runTest {
+      viewModel(
+          savedStateHandle = SavedStateHandle(mapOf(locationIdParam to 33L)),
+          getLocationById =
+            mockk<GetLocationById>().apply { coEvery { this@apply(any()) } returns null }
+        )
+        .mapPositionFlow
+        .test {
+          runCurrent()
+          assertMapPositionEquals(MapPosition(), awaitItem())
+          cancelAndIgnoreRemainingEvents()
+        }
     }
 
   @Test
@@ -70,6 +106,65 @@ class LocationViewModelTests {
           )
           cancelAndIgnoreRemainingEvents()
         }
+    }
+
+  @Test
+  fun `WHEN nothing is called THEN loading flow should emit false`() = runTest {
+    viewModel().loadingFlow.test {
+      runCurrent()
+      assertEquals(false, awaitItem())
+      cancelAndIgnoreRemainingEvents()
+    }
+  }
+
+  @Test
+  fun `WHEN nothing is called THEN save location should emit false`() = runTest {
+    viewModel().locationSavedFlow.test {
+      runCurrent()
+      expectNoEvents()
+    }
+  }
+
+  @Test
+  fun `GIVEN no initial location id WHEN save location is called THEN save location flow should emit Ready`() =
+    runTest {
+      with(
+        viewModel(
+          saveLocationUseCase =
+            mockk<SaveLocationUseCase>().apply {
+              coEvery { this@apply(any(), any(), any()) } returns Unit
+            }
+        )
+      ) {
+        locationSavedFlow.test {
+          runCurrent()
+          saveLocation(0.0, 0.0, "")
+          assertEquals(Ready(Unit), awaitItem())
+          cancelAndIgnoreRemainingEvents()
+        }
+      }
+    }
+
+  @Test
+  fun `GIVEN no initial location id WHEN save location is called THEN loading flow should emit false, true, false`() =
+    runTest {
+      with(
+        viewModel(
+          saveLocationUseCase =
+            mockk<SaveLocationUseCase>().apply {
+              coEvery { this@apply(any(), any(), any()) } returns Unit
+            }
+        )
+      ) {
+        loadingFlow.test {
+          runCurrent()
+          saveLocation(0.0, 0.0, "")
+          assertEquals(false, awaitItem())
+          assertEquals(true, awaitItem())
+          assertEquals(false, awaitItem())
+          cancelAndIgnoreRemainingEvents()
+        }
+      }
     }
 
   private fun viewModel(
