2ab1d70d6526a4eac9792748e65f531f34c65b3c
Tomasz Osoba <tomasz.osoba@knowit.eu>
2023-10-11 11:12:15 +0200

    downgrade runtime tracing version to fix dependency version related runtime crash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,8 +59,6 @@ dependencies {
   implementation(project(":widget"))
   implementation(project(":work"))
 
-  implementation(libs.accompanist.pagerindicators)
-
   implementation(libs.androidx.activity.compose)
   implementation(libs.androidx.appcompat)
   implementation(libs.androidx.core.ktx)
