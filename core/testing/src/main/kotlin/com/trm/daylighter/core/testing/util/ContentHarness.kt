bd1a341153b845d33da31d4a51a722a0bc90eca6
tosoba <therealmerengue@gmail.com>
2023-10-13 21:50:58 +0200

    provide LocalConfiguration inside setContentHarness

--- core/testing/src/main/kotlin/com/trm/daylighter/core/testing/util/ContentHarness.kt
+++ core/testing/src/main/kotlin/com/trm/daylighter/core/testing/util/ContentHarness.kt
@@ -1,10 +1,12 @@
 package com.trm.daylighter.core.testing.util
 
+import android.content.res.Configuration
 import androidx.activity.ComponentActivity
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.test.junit4.AndroidComposeTestRule
 import androidx.compose.ui.unit.DpSize
 import com.google.accompanist.testharness.TestHarness
@@ -22,7 +24,12 @@ fun <R : TestRule, A : ComponentActivity> AndroidComposeTestRule<R, A>.setConten
       val sizeClass = WindowSizeClass.calculateFromSize(size)
       CompositionLocalProvider(
         LocalWidthSizeClass provides sizeClass.widthSizeClass,
-        LocalHeightSizeClass provides sizeClass.heightSizeClass
+        LocalHeightSizeClass provides sizeClass.heightSizeClass,
+        LocalConfiguration provides
+          Configuration().apply {
+            screenWidthDp = size.width.value.toInt()
+            screenHeightDp = size.height.value.toInt()
+          }
       ) {
         composable()
       }
