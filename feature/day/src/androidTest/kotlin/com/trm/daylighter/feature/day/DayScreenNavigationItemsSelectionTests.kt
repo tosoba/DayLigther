be0279117219be64d7dea2f1652a7234909ad094
tosoba <therealmerengue@gmail.com>
2023-10-28 09:56:22 +0200

    add givenReadyChange_thenNavigationItemsCanBeSelectedOnClick test

--- feature/day/src/androidTest/kotlin/com/trm/daylighter/feature/day/DayScreenNavigationItemsSelectionTests.kt
+++ feature/day/src/androidTest/kotlin/com/trm/daylighter/feature/day/DayScreenNavigationItemsSelectionTests.kt
@@ -4,6 +4,7 @@ import androidx.activity.ComponentActivity
 import androidx.compose.ui.test.assertIsNotSelected
 import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.performClick
 import com.trm.daylighter.core.testing.util.onNodeWithEnumTestTag
 import com.trm.daylighter.feature.day.composable.ReadyChangeTestDayScreen
 import io.mockk.every
@@ -48,6 +49,28 @@ class DayScreenNavigationItemsSelectionTests {
       setContent { ReadyChangeTestDayScreen() }
       onNodeWithEnumTestTag(DayTestTags.NAVIGATION_SUNRISE_ITEM).assertIsNotSelected()
       onNodeWithEnumTestTag(DayTestTags.NAVIGATION_SUNSET_ITEM).assertIsSelected()
+    }
+  }
+
+  @Test
+  fun givenReadyChange_thenNavigationItemsCanBeSelectedOnClick() {
+    every { LocalTime.now(any<ZoneId>()) } returns LocalTime.NOON.minusHours(1L)
+
+    with(composeTestRule) {
+      setContent { ReadyChangeTestDayScreen() }
+
+      onNodeWithEnumTestTag(DayTestTags.NAVIGATION_SUNRISE_ITEM).assertIsSelected()
+      onNodeWithEnumTestTag(DayTestTags.NAVIGATION_SUNSET_ITEM).assertIsNotSelected()
+
+      onNodeWithEnumTestTag(DayTestTags.NAVIGATION_SUNSET_ITEM).performClick()
+
+      onNodeWithEnumTestTag(DayTestTags.NAVIGATION_SUNRISE_ITEM).assertIsNotSelected()
+      onNodeWithEnumTestTag(DayTestTags.NAVIGATION_SUNSET_ITEM).assertIsSelected()
+
+      onNodeWithEnumTestTag(DayTestTags.NAVIGATION_SUNRISE_ITEM).performClick()
+
+      onNodeWithEnumTestTag(DayTestTags.NAVIGATION_SUNRISE_ITEM).assertIsSelected()
+      onNodeWithEnumTestTag(DayTestTags.NAVIGATION_SUNSET_ITEM).assertIsNotSelected()
     }
   }
 }
