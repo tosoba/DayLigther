a1fab3598a8c69626e9d2c5c95337629b0e83342
tosoba <therealmerengue@gmail.com>
2023-11-01 12:27:50 +0100

    add OSM credit + about drawables

--- feature/about/src/main/kotlin/com/trm/daylighter/feature/about/AboutScreen.kt
+++ feature/about/src/main/kotlin/com/trm/daylighter/feature/about/AboutScreen.kt
@@ -1,16 +1,27 @@
 package com.trm.daylighter.feature.about
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.trm.daylighter.core.common.R as commonR
 import com.trm.daylighter.core.ui.composable.DayLighterTopAppBar
@@ -19,7 +30,7 @@ import com.trm.daylighter.core.ui.util.usingPermanentNavigationDrawer
 
 const val aboutRoute = "about_route"
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun AboutScreen(modifier: Modifier = Modifier, onDrawerMenuClick: () -> Unit) {
   Column(modifier = modifier) {
@@ -37,13 +48,37 @@ fun AboutScreen(modifier: Modifier = Modifier, onDrawerMenuClick: () -> Unit) {
         Modifier.fillMaxWidth()
           .weight(1f)
           .verticalScroll(rememberScrollState())
-          .padding(horizontal = 20.dp, vertical = 10.dp)
+          .padding(vertical = 10.dp)
     ) {
-      Text(text = stringResource(R.string.credits), style = MaterialTheme.typography.headlineSmall)
+      Text(
+        text = stringResource(R.string.credits),
+        style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.SemiBold),
+        modifier = Modifier.padding(horizontal = 20.dp, vertical = 10.dp)
+      )
+
+      Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.clickable {}.padding(horizontal = 20.dp, vertical = 10.dp)
+      ) {
+        Image(
+          painter = painterResource(R.drawable.open_street_map_creditor),
+          contentDescription = stringResource(R.string.open_street_map),
+          modifier = Modifier.size(30.dp)
+        )
+
+        Spacer(modifier = Modifier.width(10.dp))
+
+        Text(
+          text = stringResource(R.string.open_street_map),
+          style = MaterialTheme.typography.bodyLarge,
+          modifier = Modifier.weight(1f).basicMarquee()
+        )
+      }
 
       Text(
         text = stringResource(R.string.support_me),
-        style = MaterialTheme.typography.headlineSmall
+        style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.SemiBold),
+        modifier = Modifier.padding(horizontal = 20.dp, vertical = 10.dp)
       )
     }
   }
