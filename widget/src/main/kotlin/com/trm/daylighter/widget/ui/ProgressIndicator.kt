cdb984c009118f1c69f0a1b06c098eb5cfce2691
tosoba <therealmerengue@gmail.com>
2024-01-04 21:53:31 +0100

    using LinearProgressIndicator in widget

--- widget/src/main/kotlin/com/trm/daylighter/widget/ui/ProgressIndicator.kt
+++ widget/src/main/kotlin/com/trm/daylighter/widget/ui/ProgressIndicator.kt
@@ -2,17 +2,18 @@ package com.trm.daylighter.widget.ui
 
 import androidx.compose.runtime.Composable
 import androidx.glance.GlanceModifier
-import androidx.glance.appwidget.CircularProgressIndicator
+import androidx.glance.appwidget.LinearProgressIndicator
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.fillMaxWidth
 import com.trm.daylighter.core.ui.model.DayPeriodChartMode
 
 @Composable
 internal fun ProgressIndicator(chartMode: DayPeriodChartMode) {
   AppWidgetBox(
     modifier = GlanceModifier.fillMaxSize().chartBackground(chartMode),
-    contentAlignment = Alignment.Center
+    contentAlignment = Alignment.BottomCenter
   ) {
-    CircularProgressIndicator(color = GlanceTheme.colors.onPrimary)
+    LinearProgressIndicator(modifier = GlanceModifier.fillMaxWidth())
   }
 }
