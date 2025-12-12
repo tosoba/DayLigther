e67e3a2e18f5921a8d8b4049b823164a1c2cb99c
tosoba <therealmerengue@gmail.com>
2025-12-12 17:53:22 +0100

    InfoButtonCard UI cleanup

--- core/ui/src/main/kotlin/com/trm/daylighter/core/ui/composable/InfoButtonCard.kt
+++ core/ui/src/main/kotlin/com/trm/daylighter/core/ui/composable/InfoButtonCard.kt
@@ -5,8 +5,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,7 +15,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 @Composable
 fun InfoButtonCard(
@@ -29,9 +29,15 @@ fun InfoButtonCard(
       horizontalAlignment = Alignment.CenterHorizontally,
       modifier = Modifier.padding(16.dp),
     ) {
-      Text(text = infoText, fontSize = 20.sp, textAlign = TextAlign.Center)
+      Text(
+        text = infoText,
+        style = MaterialTheme.typography.headlineSmall,
+        textAlign = TextAlign.Center,
+      )
+
       Spacer(modifier = Modifier.height(12.dp))
-      OutlinedButton(onClick = onButtonClick) { Text(text = actionText) }
+
+      Button(onClick = onButtonClick) { Text(text = actionText) }
     }
   }
 }
