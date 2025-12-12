129f0a1878d9a1f752039179a6ee5ab807f3b5e0
tosoba <therealmerengue@gmail.com>
2025-12-12 18:12:34 +0100

    Replace deprecated back arrow icon

--- feature/location/src/main/kotlin/com/trm/daylighter/feature/location/LocationScreen.kt
+++ feature/location/src/main/kotlin/com/trm/daylighter/feature/location/LocationScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.material3.windowsizeclass.WindowHeightSizeClass
@@ -399,7 +400,7 @@ private fun LocationAppBar(
         modifier = Modifier.padding(start = 4.dp, top = 4.dp),
       ) {
         Icon(
-          imageVector = Icons.Filled.ArrowBack,
+          imageVector = Icons.AutoMirrored.Filled.ArrowBack,
           contentDescription = stringResource(commonR.string.back),
           tint = MaterialTheme.colorScheme.onSurface,
         )
