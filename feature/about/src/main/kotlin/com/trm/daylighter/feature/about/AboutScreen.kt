cdc3776f1065806248e43a890c78897f030abbde
tosoba <therealmerengue@gmail.com>
2025-12-18 17:33:15 +0100

    Cleanup AboutScreen

--- feature/about/src/main/kotlin/com/trm/daylighter/feature/about/AboutScreen.kt
+++ feature/about/src/main/kotlin/com/trm/daylighter/feature/about/AboutScreen.kt
@@ -2,7 +2,6 @@ package com.trm.daylighter.feature.about
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.basicMarquee
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,6 +12,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -23,13 +23,12 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import com.trm.daylighter.core.common.R as commonR
 import com.trm.daylighter.core.common.util.ext.goToUrlInBrowser
 import com.trm.daylighter.core.ui.composable.DayLighterTopAppBar
 import com.trm.daylighter.core.ui.composable.DrawerMenuIconButton
 import com.trm.daylighter.core.ui.util.usingPermanentNavigationDrawer
+import com.trm.daylighter.core.common.R as commonR
 
 const val aboutRoute = "about_route"
 
@@ -46,43 +45,33 @@ fun AboutScreen(modifier: Modifier = Modifier, onDrawerMenuClick: () -> Unit) {
       },
     )
 
-    Column(
-      modifier =
-        Modifier.fillMaxWidth()
-          .weight(1f)
-          .verticalScroll(rememberScrollState())
-          .padding(vertical = 12.dp)
-    ) {
+    Column(modifier = Modifier.fillMaxWidth().weight(1f).verticalScroll(rememberScrollState())) {
       val context = LocalContext.current
 
-      Text(
-        text = stringResource(R.string.repository),
-        style = MaterialTheme.typography.headlineSmall.copy(fontWeight = FontWeight.SemiBold),
-        modifier = Modifier.padding(horizontal = 16.dp, vertical = 12.dp),
-      )
+      Spacer(modifier = Modifier.height(12.dp))
 
-      ImageTextRow(
+      SectionTitle(text = stringResource(R.string.repository))
+
+      Spacer(modifier = Modifier.height(12.dp))
+
+      LinkCard(
         imagePainter = painterResource(R.drawable.github),
         text = stringResource(R.string.github),
         contentDescription = stringResource(R.string.github),
-        modifier =
-          Modifier.clickable { context.goToUrlInBrowser("https://github.com/tosoba/DayLigther") }
-            .padding(horizontal = 16.dp, vertical = 12.dp),
+        onClick = { context.goToUrlInBrowser("https://github.com/tosoba/DayLigther") },
       )
 
-      Text(
-        text = stringResource(R.string.credits),
-        style = MaterialTheme.typography.headlineSmall.copy(fontWeight = FontWeight.SemiBold),
-        modifier = Modifier.padding(horizontal = 16.dp, vertical = 12.dp),
-      )
+      Spacer(modifier = Modifier.height(12.dp))
 
-      ImageTextRow(
+      SectionTitle(text = stringResource(R.string.credits))
+
+      Spacer(modifier = Modifier.height(12.dp))
+
+      LinkCard(
         imagePainter = painterResource(R.drawable.open_street_map_creditor),
         text = stringResource(R.string.open_street_map),
         contentDescription = stringResource(R.string.open_street_map),
-        modifier =
-          Modifier.clickable { context.goToUrlInBrowser("https://www.openstreetmap.org/") }
-            .padding(horizontal = 16.dp, vertical = 12.dp),
+        onClick = { context.goToUrlInBrowser("https://www.openstreetmap.org/") },
       )
 
       Spacer(modifier = Modifier.height(12.dp))
@@ -91,25 +80,44 @@ fun AboutScreen(modifier: Modifier = Modifier, onDrawerMenuClick: () -> Unit) {
 }
 
 @Composable
-private fun ImageTextRow(
-  modifier: Modifier = Modifier,
+private fun SectionTitle(text: String) {
+  Text(
+    text = text,
+    style = MaterialTheme.typography.titleSmall,
+    modifier = Modifier.padding(horizontal = 16.dp),
+  )
+}
+
+@Composable
+private fun LinkCard(
   text: String,
   contentDescription: String? = null,
   imagePainter: Painter,
+  onClick: () -> Unit,
 ) {
-  Row(verticalAlignment = Alignment.CenterVertically, modifier = modifier) {
-    Image(
-      painter = imagePainter,
-      contentDescription = contentDescription,
-      modifier = Modifier.size(32.dp),
-    )
+  Card(modifier = Modifier.padding(horizontal = 16.dp), onClick = onClick) {
+    Spacer(modifier = Modifier.height(12.dp))
 
-    Spacer(modifier = Modifier.width(12.dp))
+    Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxWidth()) {
+      Spacer(modifier = Modifier.width(12.dp))
 
-    Text(
-      text = text,
-      style = MaterialTheme.typography.bodyLarge,
-      modifier = Modifier.weight(1f).basicMarquee(iterations = Int.MAX_VALUE),
-    )
+      Image(
+        painter = imagePainter,
+        contentDescription = contentDescription,
+        modifier = Modifier.size(32.dp),
+      )
+
+      Spacer(modifier = Modifier.width(12.dp))
+
+      Text(
+        text = text,
+        style = MaterialTheme.typography.bodyLarge,
+        modifier = Modifier.weight(1f).basicMarquee(iterations = Int.MAX_VALUE),
+      )
+
+      Spacer(modifier = Modifier.width(12.dp))
+    }
+
+    Spacer(modifier = Modifier.height(12.dp))
   }
 }
