1ab425d00eedd4f2efd9bd462130fa81edaa1fc9
tosoba <therealmerengue@gmail.com>
2025-12-18 17:14:10 +0100

    Remove unused methods from GlanceKtx

--- widget/src/main/kotlin/com/trm/daylighter/widget/ui/GlanceKtx.kt
+++ widget/src/main/kotlin/com/trm/daylighter/widget/ui/GlanceKtx.kt
@@ -17,10 +17,6 @@ import androidx.glance.appwidget.cornerRadius
 import androidx.glance.background
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Box
-import androidx.glance.layout.Column
-import androidx.glance.layout.ColumnScope
-import androidx.glance.layout.Row
-import androidx.glance.layout.RowScope
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.padding
 import com.trm.daylighter.core.common.di.provider.ClassProvider
@@ -34,36 +30,6 @@ internal fun AppWidgetBox(
   Box(
     modifier = appWidgetBackgroundModifier().then(modifier),
     contentAlignment = contentAlignment,
-    content = content,
-  )
-}
-
-@Composable
-internal fun AppWidgetColumn(
-  modifier: GlanceModifier = GlanceModifier,
-  verticalAlignment: Alignment.Vertical = Alignment.Top,
-  horizontalAlignment: Alignment.Horizontal = Alignment.Start,
-  content: @Composable ColumnScope.() -> Unit,
-) {
-  Column(
-    modifier = appWidgetBackgroundModifier().then(modifier),
-    verticalAlignment = verticalAlignment,
-    horizontalAlignment = horizontalAlignment,
-    content = content,
-  )
-}
-
-@Composable
-internal fun AppWidgetRow(
-  modifier: GlanceModifier = GlanceModifier,
-  verticalAlignment: Alignment.Vertical = Alignment.Top,
-  horizontalAlignment: Alignment.Horizontal = Alignment.Start,
-  content: @Composable RowScope.() -> Unit,
-) {
-  Row(
-    modifier = appWidgetBackgroundModifier().then(modifier),
-    verticalAlignment = verticalAlignment,
-    horizontalAlignment = horizontalAlignment,
     content = content,
   )
 }
@@ -85,24 +51,12 @@ internal fun GlanceModifier.appWidgetBackgroundCornerRadius(): GlanceModifier {
   return this
 }
 
-internal fun GlanceModifier.appWidgetInnerCornerRadius(): GlanceModifier {
-  if (Build.VERSION.SDK_INT >= 31) {
-    cornerRadius(android.R.dimen.system_app_widget_inner_radius)
-  } else {
-    cornerRadius(8.dp)
-  }
-  return this
-}
-
 @Composable
 internal fun stringResource(@StringRes id: Int, args: List<Any> = emptyList()): String =
   LocalContext.current.getString(id, *args.toTypedArray())
 
 internal val Float.toPx: Float
   get() = this * Resources.getSystem().displayMetrics.density
-
-internal val Float.toDp: Float
-  get() = this / Resources.getSystem().displayMetrics.density
 
 @Composable
 internal fun deepLinkAction(uri: Uri): Action =
